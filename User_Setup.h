e3a351361aad239013711dd5e1b6788c5cc02c79
monitmanllc <94817545+monitmanllc@users.noreply.github.com>
2025-12-26 23:15:18 -0800

    Create User_Setup.h

---
+++ User_Setup.h
@@ -0,0 +1,9 @@
+#define ST7789_DRIVER
+#define TFT_WIDTH  135
+#define TFT_HEIGHT 240
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   5
+#define TFT_DC   16
+#define TFT_RST  17
+#define TFT_BL   4
